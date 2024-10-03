a6835f968dde1b729bcaa2e428b5d38430f6dbcf
notandriiafanasiev <andreykrazim@gmail.com>
2024-10-03 10:37:19 +0300

    Fixed product container

--- script/index.js
+++ script/index.js
@@ -1704,6 +1704,15 @@ if (selectElement) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  // Call the updateFavoriteCount function when the page loads
+
+  const scrollContainer = document.querySelector('.product__main-content');
+  if(scrollContainer){
+    scrollContainer.scrollTo({
+      left: (scrollContainer.scrollWidth - scrollContainer.clientWidth) / 2,
+      behavior: 'smooth'
+  });
+  }
+
+
   updateFavoriteCount();
 });
